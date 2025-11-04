2582a6f1598ba16a87ab523807d96ab03c1d9154
kim-revolution-web <kimgiminv@gmail.com>
2025-11-04 22:37:54 +0900

    Update 4-12.c

--- C/chapter04/4-12.c
+++ C/chapter04/4-12.c
@@ -1,18 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
-
+//조건 연산자
 int main() {
 
 	int a = 10, b = 20, res;
 
-	res = (a > b) ? a : b;
+	res = (a > b) ? a : b; // a와 b 중에 큰 값이 res에 저장
 
 	printf("큰값 %d:\n", res);
-	
-	
+
+
 	/*if (a > b) {
-		
+
 		res = a;
 	}
 	else {
