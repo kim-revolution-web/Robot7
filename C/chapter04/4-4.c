7b8843c34237af084096e300e511817e87503c9d
kim-revolution-web <kimgiminv@gmail.com>
2025-11-04 21:37:56 +0900

    Update 4-4.c

--- C/chapter04/4-4.c
+++ C/chapter04/4-4.c
@@ -1,19 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
+//전위 표기와 후위 쵸기를 사용한 증감 연산
 
 int main() {
 
 	int a = 5, b = 5;
 	int pre, post;
 
-	pre = (++a)*3; //�������̶� �����ϰ� ���
-	post = (b++)*3;// �������̶� ��� �� ���� 
+	pre = (++a)*3; //전위형이라 증가하고 계산
+	post = (b++)*3;// 후위형이라 계산 후 증가 
 
-	//pre = (++a);, post = (b++); �̷��� ������ �����ڰ� ���ٿ� ���� ���̸� �ȵ�
+	//pre = (++a);, post = (b++); 이렇게 증가감 연산자가 한줄에 같이 쓰이면 안됨
 
 	printf("%d,%d\n",a,b);
 	printf("%d,%d\n,",pre,post);
 
 	return 0;
+
 }
