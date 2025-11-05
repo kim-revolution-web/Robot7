567fa571658866d4f9a66862b2f854c7e9217267
kim-revolution-web <kimgiminv@gmail.com>
2025-11-05 19:33:11 +0900

    Update and rename quiz4.c to A~Z.c

--- C/chapter06/A~Z.c
+++ C/chapter06/A~Z.c
@@ -1,21 +1,22 @@
 #define _CRT_SECURE_NO_WANRINGS
 #include <stdio.h>
 
+//A~Z
 int main() {
 
 
 	int a = 3;
-	int i=1;
+	int i = 1;
 	/*int sum = 0;*/
 
 	while (i < 10) {
-		
+
 		/*sum=a*i ;*/
-	printf("%d*%d = %d\n",a,i ,a*i);
-	i++;
+		printf("%d*%d = %d\n", a, i, a * i);
+		i++;
 
 	}
-
+	//A~Z 까지 아스키코드 번호로 만들기
 	/*char b = 'A';
 
 	for (i = 0; i <= 25; i++) {
@@ -23,8 +24,8 @@ int main() {
 		printf("%c", sum);
 
 	}*/
-	
-	
+
+	//A~Z까지 문자로 넣어서 만들기
 	for (i = 'A'; i <= 'Z'; i++) {
 		printf("%c", i);
 	}
@@ -35,4 +36,5 @@ int main() {
 	}
 
 	return 0;
+
 }
