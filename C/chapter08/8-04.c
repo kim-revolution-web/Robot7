0025a346a34ac11fc2d1167e663e841fe97b8080
kim-revolution-web <kimgiminv@gmail.com>
2025-11-06 09:38:07 +0900

    Create 8-04.c

---
+++ C/chapter08/8-04.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//문자열을 저장하는 char형 배열
+int main() {
+
+	char str[80] ="applejam"; // 문자열 초기화
+
+	printf("최초 문자열 : %s\n",str); // 초기화 문자열 출력
+	printf("문자열 입력 :");
+	scanf("%s",str); //새로운 문자열 입력
+	printf("입력 후 문자열 : %s\n", str); //입력된 문자열 출력
+
+
+
+	return 0;
+}
