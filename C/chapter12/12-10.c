53b99497bd8bd3d146e59df75cf0d382910660d7
kim-revolution-web <kimgiminv@gmail.com>
2025-11-07 14:21:06 +0900

    Update 12-10.c

--- C/chapter12/12-10.c
+++ C/chapter12/12-10.c
@@ -5,14 +5,16 @@
 //strcat,strncat  함수를 사용한 문자열 붙이기
 int main() {
 	
-	char str[80] = "straw";
+	char str[80] = "straw"; //문자열 초기화
 
-	strcat(str, "brerry");
+	strcat(str, "brerry"); // str 배열에 문자열 붙이기
 	printf("%s\n", str);
-	strncat(str, "piece", 3);
+	strncat(str, "piece", 3); //str 배열에 3개의 문자 붙이기
 	printf("%s\n", str);
 
 
 	return 0;
 }
+
+
 
