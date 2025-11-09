6ca4f6029529820d12b79b5ec73401d062dd439a
kim-revolution-web <kimgiminv@gmail.com>
2025-11-09 20:41:17 +0900

    Update 11-2.c

--- C/chapter11/11-2.c
+++ C/chapter11/11-2.c
@@ -1,20 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//�����̳� ���� ������ �Է�
+//공백이나 제어 문자의 입력
 int main() {
 
 	char ch1, ch2;
 
-		scanf("%c%c", &ch1, &ch2);
-		// 2���� ���ڸ� ���� �Է�
-		printf("[%c%c]\n", ch1, ch2);
-		//�Էµ� ���� ���
-		scanf("%c %c", &ch1, &ch2);
-		// ȭ��Ʈ �����̽�
-		printf("ȭ��Ʈ �����̽�[%c%c]\n", ch1, ch2);
-		
-		fflush(stdin); // TC(XXXX); �ذ� �Ұ����ϴ�
+	//scanf("%c%c", &ch1, &ch2);
+	//// 2개의 문자를 연속 입력
+	//printf("[%c%c]\n", ch1, ch2);
+	//// 띄어쓰하면 문자 하나 출력이 안됨
 
-	return 0; 
+//----------------------------------
+	//입력된 문자 출력
+	scanf("%c %c", &ch1, &ch2);
+	// 화이트 스페이스
+	printf("화이트 스페이스[%c%c]\n", ch1, ch2);
+
+	fflush(stdin); // TC(XXXX); 해결 불가능하다
+
+	return 0;
 }
