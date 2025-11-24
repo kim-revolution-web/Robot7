85ee5539ec9f11b8631e7057f4d6840e59b89ebe
kim-revolution-web <kimgiminv@gmail.com>
2025-11-24 14:00:04 +0900

    Update 19-2.c

--- C/chapter19/19-2.c
+++ C/chapter19/19-2.c
@@ -1,15 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
-#define PI 3.14159
-#define LIMIT 100.0
-#define MSG "passde!"
-#define ERR_PRN printf("������ ������ϴ�!\n")
+#define PI 3.14159 // 상수를 매크로명으로 정의
+#define LIMIT 100.0 // 상수를 매크로명으로 정의
+#define MSG "passed!" //문자열을 매크로명으로 저의
+#define ERR_PRN printf("범위를 벗어났습니다!\n") // 출력문을 매크로명으로 정의
 
-// �پ��� ��ũ�θ��� ���
+// 다양한 매크로명의 사용
 int main() {
-	double radius, area;
+	double radius, area; //반지름과 면적 변수
 
-	printf("�������� �Է»翡��(100 ����):");
+	printf("반지름을 입력사에요(100 이하):");
+	scanf("%lf", &radius); //반지름 입력
+	area = PI * radius * radius;//면적 계산
+	if (radius > LIMIT)ERR_PRN;// 반지름이 100을 초과하면 오류 메시지 출력
+	else printf("원의 면적:%.2lf (%s)\n", area, MSG); // 면적과 메시지 출력
 
 
 	
