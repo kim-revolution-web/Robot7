9effdb625dd620d67f27fb99997aabad5fc26a3d
kim-revolution-web <kimgiminv@gmail.com>
2025-12-09 10:15:47 +0900

    Refactor character dictionary creation logic

--- python/12-08/p248-2.py
+++ python/12-08/p248-2.py
@@ -2,9 +2,13 @@ key_list=["name","hp","mp","level"]
 value_list=["기사","200",30,5]
 character={}
 
-for i in key_list:
-  for j in value_list:
-    character[i]=j
-   
-   
+# for i in range(len(key_list)):
+#  key=key_list[i]
+#  value=value_list[i]
+#  character[key]=value
+# print(character)
+
+for i in range(len(key_list)):
+    character[key_list[i]]=value_list[i]
+
 print(character)
