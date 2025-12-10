22d7e57e75da7dd12a90ed3465ee986ccf94d520
kim-revolution-web <kimgiminv@gmail.com>
2025-12-10 16:31:12 +0900

    Add files via upload

---
+++ python/12-05/2-3.py
@@ -0,0 +1,13 @@
+print("5+7=",5+7)
+print("5/7=",5/7)
+print(5//7)
+
+string="문자열"
+number = 273
+#string+number#type 다르면 힘듬
+#print(int("52.273")) #type이 다름
+number=input("1.숫자를 입력하세요\n")
+print(type(number))
+number=int(input("2.숫자를 입력하세요\n"))
+print(type(number))
+
